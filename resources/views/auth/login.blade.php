9f69daa2350acd0b089aca03591540a967d0d124
bonaventure ouedraogo <bona555@live.fr>
2024-05-11 10:21:38 +0000

    table boostrap installer avec succes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,11 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                  
+              <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Inscription</a>
+        </div>
+
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
